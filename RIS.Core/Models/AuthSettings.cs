afc4ab9d7a2c0c85e5b005c3fa603174ecc17cbc
Кипоров Владислав <KiporovVA@tomskasu.ru>
2025-04-20 14:47:47 +0700

    Добавлена авторизация

---
+++ RIS.Core/Models/AuthSettings.cs
@@ -0,0 +1,8 @@
+namespace RIS.Core.Models;
+
+public class AuthSettings
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public bool IsFirstLogin { get; set; }
+}
