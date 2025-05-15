31d685d32c002474efb345f019abe2cf2d69461c
Кипоров Владислав <KiporovVA@tomskasu.ru>
2025-05-16 00:57:23 +0700

    Добавил ендпоинты, изменил Inventory.cs

--- RentalPoint/RP.Data/Data/Migrations/20250515175809_AddStatusInInventory.Designer.cs
+++ RentalPoint/RP.Data/Data/Migrations/20250515175809_AddStatusInInventory.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Data.Migrations
 {
     [DbContext(typeof(DbContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20250515175809_AddStatusInInventory")]
+    partial class AddStatusInInventory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.15");
---
+++ RentalPoint/RP.Data/Data/Migrations/20250515175809_AddStatusInInventory.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusInInventory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StatusId",
+                table: "Inventories",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_StatusId",
+                table: "Inventories",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventories_DictionaryValues_StatusId",
+                table: "Inventories",
+                column: "StatusId",
+                principalTable: "DictionaryValues",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventories_DictionaryValues_StatusId",
+                table: "Inventories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inventories_StatusId",
+                table: "Inventories");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Inventories");
+        }
+    }
+}
